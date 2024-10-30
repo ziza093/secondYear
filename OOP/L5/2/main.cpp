b1f65017339fbe1ae90f61861ca6575f7d4a818e
ziza093 <gdiaconu093@gmail.com>
2024-10-30 19:34:39 +0200

    p2 - done

---
+++ OOP/L5/2/main.cpp
@@ -0,0 +1,19 @@
+#include "MultimeComplexe.h"
+
+int main() {
+ 
+    MultimeComplexe m(5);
+    Complex c1(2,3), c2(3,4), c3(2,-1);
+
+    m.adauga(c1);
+    m.adauga(c2);
+    m.afisare();
+    m.extrage(c1);
+    m.extrage(c3);
+    m.afisare();
+    m.adauga(c3);
+    m.adauga(c3);
+    m.afisare();
+ 
+    return 0;
+}
